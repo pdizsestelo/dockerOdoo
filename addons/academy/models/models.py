3855aab565583d97f5b94542f1d450d1a02e1116
pdizsestelo <pdizsestelo@danielcastelao.org>
2021-03-23 10:23:07 +0100

    Añadiendo la carpeta ddons con el modulo pasado por correo

---
+++ addons/academy/models/models.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Course(models.Model):
+    _name = 'openacademy.course'
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+
+
+
+# class academy(models.Model):
+#     _name = 'academy.academy'
+#     _description = 'academy.academy'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
